2b7a55c26ca75c466147ca9fc9376f0d873b1bdf
Oleksii Vodka <oleksii.vodka@gmail.com>
2023-03-26 12:08:26 +0300

    Update 2023 to new rules

--- khpi-zno-lookup.php
+++ khpi-zno-lookup.php
@@ -4,11 +4,35 @@
 Plugin Name: KhPI ZNO Lookup
 Plugin URI: http://kpi.kharkov.ua
 Description: NTU KhPI ZNO Lookup plugin
-Version: 1.0
+Version: 2.0
 Author: Oleksii Vodka
 Author URI: http://web.kpi.kharkov.ua/dpm/uk/vodka-oleksij-oleksandrovich/
 License: GPL2
 */
+
+/*
+add_action('template_redirect', function () {
+	ob_start();
+});
+*/
+
+
+
+function include_jQuery() {
+	if (!is_admin()) {
+		wp_enqueue_script('jquery');
+	}
+}
+add_action('init', 'include_jQuery');
+add_action('admin_post_edit_frm', 'khpi_zno_lookup_load_content');
+//add_action('admin_post_addnew', 'test_f');
+
+function test_f()
+{
+	echo "Hello world";
+	die(0);
+}
+
 
 define('KHPI_ZNO_LOOKUP_ADMIN_SUFFIX', 'znolookup');
 
@@ -22,7 +46,7 @@ register_activation_hook(__FILE__, 'khpi_zno_lookup_install');
 define('KHPI_ZNO_LOOKUP_TABLENAME', $wpdb->prefix . "khpi_zno_lookup");
 
 global $khpi_zno_lookup_db_version;
-$khpi_zno_lookup_db_version = "1.0";
+$khpi_zno_lookup_db_version = "2.0";
 
 function khpi_zno_lookup_install()
 {
@@ -52,6 +76,7 @@ function khpi_zno_lookup_install()
           `Certificate` double(5,2) NOT NULL DEFAULT '0',
           `PK` double(5,2) NOT NULL DEFAULT '0',
           `TK` double(5,2) NOT NULL DEFAULT '0',
+          `link` varchar(250) default  NULL,
         PRIMARY KEY (`id`)
         ) {$wpdb -> get_charset_collate()} ;
      ";
@@ -75,42 +100,52 @@ function khpi_zno_lookup_init()
     if (!current_user_can('manage_options')) {
         wp_die(__('You do not have sufficient permissions to access this page.'));
     }
-
-
     khpi_zno_lookup_load_content();
-
-
 }
 
 function khpi_zno_lookup_showlist()
 {
     global $wpdb;
     $zno_objs = $wpdb->get_results("SELECT *  FROM " . KHPI_ZNO_LOOKUP_TABLENAME, OBJECT);
-    $addlink = admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX . '&addnew=1');
+    $add_link = admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX . '&addnew=1');
+	$dwl_link = admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX . '&dwl=1');
+
+	$post_url = admin_url('admin-post.php');
+
     echo "
         <h1>KhPI ZNO Lookup Plugin</h1>
-        <h2>Спеціальності/Спеціалізації та конкурсні предмети для вступу  <a href='{$addlink}'' class='button'>Створити нову</a></h2>
+        <h2>Спеціальності/Спеціалізації та конкурсні предмети для вступу  <a href='{$add_link}'' class='button'>Додати рядок</a>
+        
+        <form action='{$post_url}' method='post'>
+			<input type='hidden' name='action' value='generate_csv'>
+			<input type='submit' value='Завантажити CSV' class='button'>
+		</form>
+        
+        </h2>
         <form  method=\"post\" enctype=\"multipart/form-data\">
                 <input type='file' id='upload_csv' name='upload_csv'/>
                 <input type='submit' value='Завантажити'>
         </form>
-        <table id='zno_table'> 
+        <table id='zno_table' width='100%'> 
         <tr>
         <th>Код</th>
         <th>Спеціальінсть</th>
-        <th>Спеціалізація</th>
-        <th>Факультет/Інститут</th>
+<!--    <th>Спеціалізація</th> -->
+        <th>ННІ</th>
         <th>Укр</th>
         <th>Мат</th>
         <th>Фіз</th>
         <th>Іст</th>
         <th>Іноз</th>
-        <th>Гео</th>
+<!--    <th>Гео</th> -->
         <th>Хім</th>
         <th>Біо</th>
+        <th>Посил</th>
+<!--    
         <th>Ат</th>
         <th>ПК</th>
         <th>Т Конк</th>
+-->
         <th>X</th>
         </tr>";
     $i = 0;
@@ -124,19 +159,20 @@ function khpi_zno_lookup_showlist()
 
             echo "<td><a href='$link'>{$obj->Code}</a></td>";
             echo "<td><a href='$link'>{$obj->Speciality}</a></td>";
-            echo "<td><a href='$link'>{$obj->Specialization}</a></td>";
+            //echo "<td><a href='$link'>{$obj->Specialization}</a></td>";
             echo "<td><a href='$link'>{$obj->faculty}</a></td>";
             echo "<td>{$obj->UkrainianLanguage}</td>";
             echo "<td>{$obj->Math}</td>";
             echo "<td>{$obj->Physics}</td>";
             echo "<td>{$obj->HistoryOfUkraine}</td>";
             echo "<td>{$obj->ForeignLanguage}</td>";
-            echo "<td>{$obj->Geography}</td>";
+			//echo "<td>{$obj->Geography}</td>";
             echo "<td>{$obj->Chemistry}</td>";
             echo "<td>{$obj->Biology}</td>";
-            echo "<td>{$obj->Certificate}</td>";
-            echo "<td>{$obj->PK}</td>";
-            echo "<td>{$obj->TK}</td>";
+            //echo "<td>{$obj->Certificate}</td>";
+            //echo "<td>{$obj->PK}</td>";
+            //echo "<td>{$obj->TK}</td>";
+	        echo "<td><a href='{$obj->link}'>Посил</a></td>";
             echo "<td><a href='{$dellink}' title='Видалити' onclick=\"return confirm('Видалити?');\">X</a></td>";
         }
         echo "</tr>";
@@ -147,7 +183,7 @@ function khpi_zno_lookup_showlist()
     <script type='application/javascript'>
     
     jQuery('#zno_table tr td').each(function(i,elem){
-        if (jQuery(elem).text()=='0.00') 
+        if (jQuery(elem).text()==='0.00') 
             jQuery(elem).text('');
         else if (parseFloat(jQuery(elem).text())<0)
         jQuery(elem).css('background','#CCCCFF');
@@ -168,15 +204,17 @@ function khpi_zno_lookup_show_edit_form()
     $zno_obj = $zno_objs[0];
     $admin_url = admin_url("admin.php?page=" . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX);
 
+	$post_url = admin_url('admin-post.php');
     echo "
     <h1>Редагувати запис:</h1>
     <em>Від’ємне значення коефіцієнту вказує, що цей предмет є обов’язковим</em> 
-    <form action='{$admin_url}' method='post' id='khpi_zno_lookup_form'>
+    <form action='{$post_url}' method='post' id='khpi_zno_lookup_form'>
         <input type='hidden' value='{$zno_obj->id}' name='edit_id'>
+        <input type='hidden' name='action' value='edit_frm'>
         <label>Код спеціальності:<br/><input type='number' value='{$zno_obj->Code}' name='Code'></label><br/>
         <label>Спеціальність:<br/><input type='text' value='{$zno_obj->Speciality}' name='Speciality'></label><br/>
         <label>Спеціалізація:<br/><input type='text' value='{$zno_obj->Specialization}' name='Specialization'></label><br/>
-        <label>Факультет/Інститут<br/><input type='text' value='{$zno_obj->faculty}' name='faculty'></label><br/>
+        <label>Інститут<br/><input type='text' value='{$zno_obj->faculty}' name='faculty'></label><br/>
         <label>Українська мова та література:<br/><input type='text' value='{$zno_obj->UkrainianLanguage}' name='UkrainianLanguage'></label><br/>
         <label>Математика:<br/><input type='text' value='{$zno_obj->Math}' name='Math'></label><br/>
         <label>Фізика:<br/><input type='text' value='{$zno_obj->Physics}' name='Physics'></label><br/>
@@ -188,6 +226,7 @@ function khpi_zno_lookup_show_edit_form()
         <label>Середній бал атестату:<br/><input type='text' value='{$zno_obj->Certificate}' name='Certificate'></label><br/>
         <label>Підкурси:<br/><input type='text' value='{$zno_obj->PK}' name='PK'></label><br/>
         <label>Творчий конкурс:<br/><input type='text' value='{$zno_obj->TK}' name='TK'></label><br/>
+        <label>Посилання:<br/><input type='url' value='{$zno_obj->link}' name='link'></label><br/>
         <input type='submit' value='Застосувати'>
     </form>
     
@@ -215,8 +254,8 @@ function khpi_zno_lookup_show_edit_form()
           }
          }         
     
-    jQuery('#khpi_zno_lookup_form label input[type!=number]').change(function(){f(this);});
-    jQuery('#khpi_zno_lookup_form label input[type!=number]').each(function(i,elem){f(elem);});
+    jQuery('#khpi_zno_lookup_form label input[type!=number][type!=url]').change(function(){f(this);});
+    jQuery('#khpi_zno_lookup_form label input[type!=number][type!=url]').each(function(i,elem){f(elem);});
     
     
     </script>
@@ -224,21 +263,77 @@ function khpi_zno_lookup_show_edit_form()
 
 }
 
+/**
+ * Generate CSV File.
+ */
+
+add_action( 'admin_post_generate_csv', 'lunchbox_generate_orders_csv' );
+function lunchbox_generate_orders_csv() {
+
+	global $wpdb;
+
+	$filename = 'spec-list';
+	$generatedDate = $generatedDate = date('d-m-Y His');
+
+	/**
+	 * output header so that file is downloaded
+	 * instead of open for reading.
+	 */
+	header("Pragma: public");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+	header("Cache-Control: private", false);
+	header('Content-Type: text/csv; charset=utf-8');
+	// header("Content-Type: application/octet-stream");
+	header("Content-Disposition: attachment; filename=\"" . $filename . " " . $generatedDate . ".csv\";" );
+	// header('Content-Disposition: attachment; filename=lunchbox_orders.csv');
+	header("Content-Transfer-Encoding: binary");
+
+	/**
+	 * create a file pointer connected to the output stream
+	 * @var [type]
+	 */
+	$output = fopen('php://output', 'w');
+
+	$results = $wpdb->get_results("SELECT *  FROM " . KHPI_ZNO_LOOKUP_TABLENAME, ARRAY_A);
+
+	//print_r($results);
+	//exit();
+
+	/**
+	 * output the column headings
+	 */
+	fputcsv( $output, array_keys($results[0]), ';');
+
+	foreach ( $results as $key => $data ) {
+		foreach ($data as $k => $v) {
+			$data[$k] = trim(mb_convert_encoding($v, "windows-1251", "UTF-8"));
+		}
+		fputcsv( $output, $data,';' );
+	}
+	return $output;
+}
+
 function khpi_zno_lookup_load_content()
 {
+
     global $wpdb;
+
     if (isset($_REQUEST['id'])) {
         khpi_zno_lookup_show_edit_form();
     } elseif (isset($_REQUEST['edit_id'])) {
+
         $id = (int)$_REQUEST['edit_id'];
         unset($_POST['edit_id']);
-        $wpdb->update(KHPI_ZNO_LOOKUP_TABLENAME, $_POST, array('id' => $id));
+	    unset($_POST['action']);
+        $updated = $wpdb->update(KHPI_ZNO_LOOKUP_TABLENAME, $_POST, array('id' => $id));
         wp_redirect(admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX));
+	    return;
 
     } elseif (isset($_REQUEST['delid'])) {
         $wpdb->delete(KHPI_ZNO_LOOKUP_TABLENAME, array('ID' => (int)$_REQUEST['delid']), array('%d'));
-        wp_redirect(admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX));
-        wp_die();
+	    khpi_zno_lookup_showlist();
+
     } elseif (isset($_REQUEST['addnew'])) {
         $wpdb->insert(KHPI_ZNO_LOOKUP_TABLENAME,
             array(
@@ -252,9 +347,7 @@ function khpi_zno_lookup_load_content()
                 '%d'
             )
         );
-
-        wp_redirect(admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX . "&id=" . $wpdb->insert_id));
-        wp_die();
+	    khpi_zno_lookup_showlist();
     } elseif (isset($_FILES['upload_csv'])) {
         if ($_FILES['upload_csv']['error'] != 0)
             wp_die("Cann't upload file");
@@ -288,19 +381,20 @@ function khpi_zno_lookup_load_content()
                 $insdata = array();
 
                 foreach ($data as $k => $v) {
-                    $insdata[$rowname[$k]] = mb_convert_encoding($v, "UTF-8", "windows-1251");;
+                    $insdata[$rowname[$k]] = trim(mb_convert_encoding($v, "UTF-8", "windows-1251"));
                 }
 
                 $wpdb->insert(KHPI_ZNO_LOOKUP_TABLENAME, $insdata);
 
             }
         }
+		else
+			wp_die("Cann't can't open file $fname");
 
 
         $wpdb->show_errors(true);
-
         unlink($fname);
-        wp_redirect(admin_url('admin.php?page=' . KHPI_ZNO_LOOKUP_ADMIN_SUFFIX));
+	    khpi_zno_lookup_showlist();
     } else {
         khpi_zno_lookup_showlist();
     }
@@ -309,20 +403,14 @@ function khpi_zno_lookup_load_content()
 
 function khpi_zno_lookup_shortcode($atts)
 {
-
-
     $content = <<<HTML
-    
 <div id="zno" class="zno_row">
     <div id='chkbox' class="zno_col-1-4">
         <p>Оберіть сертифікати ЗНО:</p>
     </div>
     <div id="spec" class="zno_col-3-4"></div>
 </div>
-
 HTML;
-
-
     return $content;
 }
 
--- khpi_zno_lookup_script.js
+++ khpi_zno_lookup_script.js
@@ -3,7 +3,7 @@ function init_khpi_zno_lookup() {
 
     $ = jQuery;
 
-    var zno_list = [];
+    let zno_list = [];
 
     zno_list['UkrainianLanguage'] = 'Українська мова та література';
     zno_list['Math'] = 'Математика';
@@ -11,13 +11,13 @@ function init_khpi_zno_lookup() {
 
     zno_list['HistoryOfUkraine'] = 'Історія України';
     zno_list['ForeignLanguage'] = 'Іноземна мова';
-    zno_list['Geography'] = 'Географія';
+    //zno_list['Geography'] = 'Географія';
 
     zno_list['Chemistry'] = 'Хімія';
     zno_list['Biology'] = 'Біологія';
-    zno_list['TK'] = 'Творчий конкурс';
+    //zno_list['TK'] = 'Творчий конкурс';
 
-    var zno_list_chk = [];
+    let zno_list_chk = [];
 
     zno_list_chk['UkrainianLanguage'] = true;
     zno_list_chk['Math'] = false;
@@ -31,14 +31,13 @@ function init_khpi_zno_lookup() {
     zno_list_chk['Biology'] = false;
     zno_list_chk['TK'] = false;
 
-    var zno_list_values = [];
+    let zno_list_values = [];
 
-    var high_spec_code = [101, 105, 124, 131, 132, 133, 141, 142, 144, 145, 151, 152, 153, 161, 162, 171, 172, 181, 185, 186, 263, 273, 274];
+    let high_spec_code = [101, 105, 124, 131, 132, 133, 141, 142, 144, 145, 161, 171, 172, 173, 174, 175, 181, 185, 186, 263, 273, 274];
 
+    let obj_data = null;
 
-    var obj_data = null;
-
-    var chkbox_arr = [];
+    let chkbox_arr = [];
 
     $.ajaxSetup({
         scriptCharset: "utf-8",
@@ -53,11 +52,11 @@ function init_khpi_zno_lookup() {
             item.id = parseInt(item.id);
             return item;
         });
-        for (var zno in zno_list) {
+        for (let zno in zno_list) {
             zno_list_values[zno] = 0;
             var elem = null;
             var elem1 = null;
-            if (zno == 'UkrainianLanguage') {
+            if (zno === 'UkrainianLanguage') {
                 elem = $("<input type='checkbox' checked disabled value='" + zno + "'/><label>" + zno_list[zno] + "</label>").appendTo("#chkbox");
                 elem1 = $("<input type='text' size='5' id = '" + zno + "' style='width:100%;'/><br/>").appendTo("#chkbox");
             }
@@ -69,14 +68,14 @@ function init_khpi_zno_lookup() {
                 zno_list_chk[$(this).val()] = $(this).prop("checked");
                 $(this).next().next()[0].disabled = !$(this).prop("checked");
 
-                var numcheck = 0;
-                for (var chk in chkbox_arr) {
+                let numcheck = 0;
+                for (let chk in chkbox_arr) {
                     if (chkbox_arr[chk].checked) {
                         numcheck++;
                     }
                 }
-                for (var chk in chkbox_arr) {
-                    if (chk == 0) continue;
+                for (let chk in chkbox_arr) {
+                    if (chk === 0) continue;
                     if (numcheck >= 3) {
                         chkbox_arr[chk].disabled = !chkbox_arr[chk].checked;
                     }
@@ -94,7 +93,7 @@ function init_khpi_zno_lookup() {
                     $(this).css('background-color', '#feb0b0');
                     zno_list_values[$(this)[0].id] = 0;
                 }
-                else if ($(this).prop('type') != 'button')
+                else if ($(this).prop('type') !== 'button')
                     $(this).css('background-color', 'inherit');
 
                 plotSpec(data);
@@ -108,7 +107,7 @@ function init_khpi_zno_lookup() {
                     $(this).css('background-color', '#feb0b0');
                     zno_list_values[$(this)[0].id] = 0;
                 }
-                else if ($(this).prop('type') != 'button')
+                else if ($(this).prop('type') !== 'button')
                     $(this).css('background-color', 'inherit');
 
                 plotSpec(data);
@@ -119,25 +118,25 @@ function init_khpi_zno_lookup() {
             elem1.change(f1);
         }
 
-        elem = $("<input type='checkbox' id='chk_certificate' value='Certificate'/><label for='chk_certificate'>Бал атестату за 12-ти бальною шкалою</label>").appendTo("#chkbox");
-        elem1 = $("<input type='text' size='5' id='certificate' disabled style='width:100%;'/><br/>").appendTo("#chkbox");
+        //elem = $("<input type='checkbox' id='chk_certificate' value='Certificate'/><label for='chk_certificate'>Бал атестату за 12-ти бальною шкалою</label>").appendTo("#chkbox");
+        //elem1 = $("<input type='text' size='5' id='certificate' disabled style='width:100%;'/><br/>").appendTo("#chkbox");
 
-        $("<input type='checkbox' id='chk_rk' value='rk' checked disabled/><label for='chk_rk'>Регіональний коефіцієнт (1,02)</label><br/>").appendTo("#chkbox");
-        $("<input type='checkbox' id='chk_sk' value='sk'/><label for='chk_sk'>Сільський коефіцієнт (1,02)</label><br/>").appendTo("#chkbox").change(f1);
-        $("<input type='checkbox' id='chk_sec' value='sec'/><label for='chk_sec'>Галузевий коефіцієнт* (1,02)</label><br/>").appendTo("#chkbox").change(f1);
+        $("<input type='checkbox' id='chk_rk' value='rk' checked disabled/><label for='chk_rk'>Регіональний коефіцієнт (1,00)</label><div style='font-size:0.75em;line-height:0.9;font-style: italic;'>(Значення буде визначено в червні 2023&nbsp;р.)</div>").appendTo("#chkbox");
+//        $("<input type='checkbox' id='chk_sk' value='sk'/><label for='chk_sk'>Сільський коефіцієнт (1,02)</label><br/>").appendTo("#chkbox").change(f1);
+        $("<input type='checkbox' id='chk_sec' value='sec'/><label for='chk_sec'>Галузевий коефіцієнт* (1,02) </label><br/>").appendTo("#chkbox").change(f1);
 
-        btn = $("<input type='button' id='ok_btn' value='Розрахувати' style='width:100%;'><br/>").appendTo("#chkbox");
+        let btn = $("<input type='button' id='ok_btn' value='Розрахувати' style='width:100%;'><br/>").appendTo("#chkbox");
 
         $("<br/><small>*Галузевий коефіцієнт нараховується лише на певні спеціальності (<a href='http://vstup.kpi.kharkov.ua/admission/admission_rules/' target='_blank'>див. перелік додаток 4</a>) за умови подання заяви з першим або другим пріорітетом</small>").appendTo("#chkbox");
 
         btn.click(f1);
-
+/*
         elem.change(function () {
             $(this).next().next()[0].disabled = !$(this).prop("checked");
             plotSpec(data);
         });
         elem1.change(f2);
-
+*/
         data.sort(function (a, b) {
             return a.Code - b.Code;
         });
@@ -146,30 +145,40 @@ function init_khpi_zno_lookup() {
     });
 
     function calcMark(data) {
-        for (var spec in data) {
-            var ukr_val = data[spec].UkrainianLanguage * zno_list_values['UkrainianLanguage'];
-            for (var s in zno_list) {
-                prof = (data[spec][s] < 0) ? s : null;
+        for (let spec in data) {
+            let s;
+            let ukr_val = data[spec].UkrainianLanguage * zno_list_values['UkrainianLanguage'];
+            for (s in zno_list) {
+                var prof = (data[spec][s] < 0) ? s : null;
                 if (prof) break;
             }
             var prof_val = Math.abs(data[spec][prof]) * zno_list_values[prof];
-            var custom_val = 0.;
-            for (var s in zno_list) {
-                if (s == 'UkrainianLanguage') continue;
-                if (data[spec][s] > 0 && zno_list_chk[s] == true) {
-                    custom_val = Math.max(custom_val, data[spec][s] * zno_list_values[s])
+            let custom_val = 0.;
+            let custom_name = "";
+            for (s in zno_list) {
+                if (s === 'UkrainianLanguage') continue;
+                if (data[spec][s] > 0 && zno_list_chk[s] === true) {
+                    if (data[spec][s] * zno_list_values[s] > custom_val)
+                    {
+                        custom_val = data[spec][s] * zno_list_values[s];
+                        custom_name = s;
+                    }
                 }
             }
-            data[spec].mark = ukr_val + prof_val + custom_val;
+            let s1 = ukr_val + prof_val + custom_val;
+            let s2 = parseFloat(data[spec].UkrainianLanguage) + Math.abs(data[spec][prof]) + parseFloat(data[spec][custom_name]);
+            data[spec].mark =  s1 / s2;
+
+            //console.log(s1,s2,data[spec].mark);
 
             if ($('#chk_certificate').prop('checked') && parseFloat($('#certificate').val()) >= 1.0 && parseFloat($('#certificate').val()) <= 12.0) {
-                Cert = 10.0 * parseFloat($('#certificate').val().replace(',', '.')) + 80.0;
+                let Cert = 10.0 * parseFloat($('#certificate').val().replace(',', '.')) + 80.0;
                 Cert = Cert < 100 ? 100 : Cert;
                 data[spec].mark += data[spec].Certificate * Cert;
             }
 
 
-            data[spec].mark *= 1.02;
+            // data[spec].mark *= 1.02;
             data[spec].mark *= $('#chk_sk').prop('checked') ? 1.02 : 1.0;
 
             if ($('#chk_sec').prop('checked') && high_spec_code.indexOf(data[spec].Code) >= 0)
@@ -189,7 +198,7 @@ function init_khpi_zno_lookup() {
         data.sort(function (a, b) {
             if (a.mark === b.mark)
                 if (a.Code === b.Code)
-                    return a.Specialization.localeCompare(b.Specialization);
+                    return 0; //a.Specialization.localeCompare(b.Specialization);
                 else
                     return a.Code - b.Code;
             else
@@ -197,35 +206,35 @@ function init_khpi_zno_lookup() {
         });
 
         $("#spec").empty();
-        $("<h2><strong>Спеціальності та спеціалізації</strong> відповідні до ваших ЗНО:</h2>").appendTo("#spec");
-        var num = 0;
-        for (var spec in data) {
-            var cond = true;
-            var prof = null;
+        $("<h2><strong>Спеціальності</strong>, відповідні до ваших НМТ:</h2>").appendTo("#spec");
+        let num = 0;
+        for (let spec in data) {
+            let cond = true;
+            let prof = null;
 
-            var other = "";
-            var other_coef = 0.;
-            var prof_coef = 0.;
+            let other = "";
+            let other_coef = 0.;
+            let prof_coef = 0.;
 
-            for (var s in zno_list) {
+            for (let s in zno_list) {
                 prof = (data[spec][s] < 0) ? s : null;
                 if (prof) break;
             }
             prof_coef = Math.abs(data[spec][prof]);
 
 
-            var num_of_match = 0;
-            for (var s in zno_list) {
-                if (s == 'UkrainianLanguage') continue;
+            let num_of_match = 0;
+            for (let s in zno_list) {
+                if (s === 'UkrainianLanguage') continue;
 
-                num_of_match += (zno_list_chk[s] == true && data[spec][s] > 0);
+                num_of_match += (zno_list_chk[s] === true && data[spec][s] > 0);
 
-                if (data[spec][s] > 0 && zno_list_chk[s] == true) {
+                if (data[spec][s] > 0 && zno_list_chk[s] === true) {
                     other += "<b>" + zno_list[s] + "; </b>";
-                    other_coef = data[spec][s];
+                    other_coef = parseFloat(data[spec][s]);
 
                 }
-                if (data[spec][s] > 0 && zno_list_chk[s] == false)
+                if (data[spec][s] > 0 && zno_list_chk[s] === false)
                     other += zno_list[s] + "; ";
 
             }
@@ -233,17 +242,18 @@ function init_khpi_zno_lookup() {
             cond = (num_of_match >= 1) && zno_list_chk[prof];
 
             if (cond) {
-                faculty = data[spec].faculty[0] + data[spec].faculty.substring(1, data[spec].faculty.length).toLocaleLowerCase();
-                total = data[spec].mark;
+                let faculty = data[spec].faculty[0] + data[spec].faculty.substring(1, data[spec].faculty.length).toLocaleLowerCase();
+                let total = data[spec].mark;
                 total = total ? total : 0;
-                code = String(data[spec].Code);
-                code = code.length == 3 ? code : ("0" + code);
-                $("<a href='#" + data[spec].Code + "' >" + code + " «" + data[spec].Speciality + "»</a> — Спеціалізація «" + data[spec].Specialization + "» <br/>Факультет «" + faculty + "»<br/>").appendTo("#spec");
-                $("<span style='font-size:0.7em;padding-left:2em;'><i>Обов'язковий</i> (" + data[spec].UkrainianLanguage + "):<b>" + zno_list['UkrainianLanguage'] + "</b></span><br/>").appendTo("#spec");
-                $("<span style='font-size:0.7em;padding-left:2em;'><i>Профільний</i> (" + prof_coef + "): <b>" + zno_list[prof] + "</b></span><br/>").appendTo("#spec");
-                $("<span style='font-size:0.7em;padding-left:2em;'><i>На вибір</i> (" + other_coef + "): " + other + "</span></br>").appendTo("#spec");
+                let code = String(data[spec].Code);
+                code = code.length === 3 ? code : ("0" + code);
+                let ukr_coef = parseFloat(data[spec].UkrainianLanguage);
+                $("<a href='" + data[spec].link + "' >" + code + " «" + data[spec].Speciality + "»</a><br/>ННІ «" + faculty + "»<br/>").appendTo("#spec");
+                $("<span style='font-size:0.7em;padding-left:2em;'><i>Обов'язковий</i> (" + ukr_coef.toFixed(2) + "):<b>" + zno_list['UkrainianLanguage'] + "</b></span><br/>").appendTo("#spec");
+                $("<span style='font-size:0.7em;padding-left:2em;'><i>Профільний</i> (" + prof_coef.toFixed(2) + "): <b>" + zno_list[prof] + "</b></span><br/>").appendTo("#spec");
+                $("<span style='font-size:0.7em;padding-left:2em;'><i>На вибір</i> (" + other_coef.toFixed(2) + "): " + other + "</span></br>").appendTo("#spec");
                 if (total > 0.)
-                    $("<span>Конкурсний бал: " + total + "</span></br>").appendTo("#spec");
+                    $("<span>Конкурсний бал: " + total.toFixed(3) + "</span></br>").appendTo("#spec");
 
                 $("</br>").appendTo("#spec");
                 num++;
